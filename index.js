3d7f55c18baa9a10c79c7163b81cd3724b5867ab
walkerle <118040893+walkerle@users.noreply.github.com>
2022-11-22 23:57:00 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Code your solutions in this file
+/*
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+}
+*/
+
+/*
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for(let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        debugger;
+    }
+    return gifts;
+}
+
+wrapGifts(gifts);
+*/
+
+const nameArray = ["Charlie", "Samip", "Ali"];
+
+function writeCards(name, eventName) {
+    let newArray = [];
+    for(let i = 0; i < name.length; i++) {
+        newArray[i] = `Thank you, ${name[i]}, for the wonderful ${eventName} gift!`
+    }
+    return newArray;
+}
+
+// console.log(nameArray);
+// console.log(writeCards(nameArray, "Christmas"));
+
+let count = 10;
+function countDown(number) {
+    while (number > 0) {
+        console.log(number--);
+    }
+    return console.log(number);
+}
+
+// countDown(count);
